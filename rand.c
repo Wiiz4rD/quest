4f1b24293f1015ff5b86b25b1e1b0daa51dfe44e
Wiiz4rD <95846812+Wiiz4rD@users.noreply.github.com>
2023-07-14 10:18:20 +0000

    Add files via upload

---
+++ rand.c
@@ -0,0 +1,36 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int local_1044;
+    int iVar1;
+    int iVar2;
+    int iVar3;
+    int iVar4;
+    int iVar5;
+    int iVar6;
+    int iVar7;
+ 
+    local_1044 = 1686261886;
+    
+    srand(local_1044);
+    iVar1 = rand();
+    iVar2 = rand();
+    iVar3 = rand();
+    iVar4 = rand();
+    iVar5 = rand();
+    iVar6 = rand();
+    iVar7 = rand();
+    
+    printf("iVar1=%d\n", iVar1);
+    printf("iVar2=%d\n", iVar2);
+    printf("iVar3=%d\n", iVar3);
+    printf("iVar4=%d\n", iVar4);
+    printf("iVar5=%d\n", iVar5);
+    printf("iVar6=%d\n", iVar6);
+    printf("iVar7=%d\n", iVar7);
+    
+
+    return 0;
+}
