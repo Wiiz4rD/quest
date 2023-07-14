66d609f7340c6aa14aec831e74c2dbedaabfd410
Wiiz4rD <95846812+Wiiz4rD@users.noreply.github.com>
2023-07-14 10:50:14 +0000

    Add files via upload

--- rand.c
+++ rand.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int local_1044;
+    long int local_1044;
     int iVar1;
     int iVar2;
     int iVar3;
@@ -24,12 +24,23 @@ int main()
     iVar7 = rand();
     
     printf("iVar1=%d\n", iVar1);
+    printf("%02x\n", iVar1 & 0xFF);
     printf("iVar2=%d\n", iVar2);
+    printf("%02x\n", iVar2 & 0xFF);
     printf("iVar3=%d\n", iVar3);
+    printf("%02x\n", iVar3 & 0xFF);
     printf("iVar4=%d\n", iVar4);
+    printf("%02x\n", iVar4 & 0xFF);
     printf("iVar5=%d\n", iVar5);
+    printf("%02x\n", iVar5 & 0xFF);
     printf("iVar6=%d\n", iVar6);
+    printf("%02x\n", iVar6 & 0xFF);
     printf("iVar7=%d\n", iVar7);
+    
+    srand(local_1044);
+    for (int i = 0 ; i < 8*(3904-8)/8; i++ ) {
+        printf("%02x, ", rand() & 0xFF);
+    }
     
 
     return 0;
